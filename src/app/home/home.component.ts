6c43a9a663789b6efffef87291ca0463cf50a14e
Vijendra-Achar <vijendra.1995@gmail.com>
2021-01-05 23:30:39 +0530

    Data tree Rendered ✔

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,10 @@
 import { ServerDataService } from './../services/server-data.service';
 import { Component, OnInit } from '@angular/core';
+import { NestedTreeControl } from '@angular/cdk/tree';
+import { Data } from '../services/data.model';
+import { MatTreeNestedDataSource } from '@angular/material/tree';
+
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,11 +12,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  treeControl = new NestedTreeControl<Data>((node) => node.children);
+  dataSource = new MatTreeNestedDataSource<Data>();
+
   constructor(private dataService: ServerDataService) {}
 
   ngOnInit(): void {
-    this.dataService.getData().subscribe((data) => {
-      console.log(data);
+    this.dataService.getData().subscribe((data: Data[]) => {
+      this.dataSource.data = data;
     });
   }
+
+  hasChild = (_: number, node: FoodNode) =>
+    !!node.children && node.children.length > 0;
 }
---
+++ src/app/services/data.model.ts
@@ -0,0 +1,12 @@
+export interface Data {
+  name: string;
+  id: string;
+  children?: Array<any>;
+}
+
+interface childern {
+  name: string;
+  startButton: string;
+  stopButton: string;
+  id: string;
+}
